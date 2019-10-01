bd9c46bf4c95c7c65e25c4f9e2b56d5bd193ee36
Delshad <Delshadk1@gmail.com>
2019-10-01 11:45:32 +0200

    First commit

---
+++ www/index.js
@@ -0,0 +1,14 @@
+
+let body = document.querySelector('body')
+let CreateWrapper = document.createElement('div');
+body.append(CreateWrapper);
+
+let elem = document.querySelector('div');
+elem.style.backgroundColor = 'lightyellow';
+elem.style.display = 'flex';
+elem.style.justifyContent = 'center';
+elem.style.minHeight = '500px';
+
+
+
+
